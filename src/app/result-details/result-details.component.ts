2db22026f8f7fd6c39c07b8545c05fe64774e3be
warrantEagle <aro_1595@wp.pl>
2018-06-15 23:43:49 +0200

    Result details and login update.

---
+++ src/app/result-details/result-details.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {DataService} from '../data.service';
+import {ActivatedRoute, Params} from '@angular/router';
+import { Location } from '@angular/common';
+import { switchMap } from 'rxjs/operators';
+import {Result} from '../result';
+
+@Component({
+  selector: 'app-result-details',
+  templateUrl: './result-details.component.html',
+  styleUrls: ['./result-details.component.css']
+})
+export class ResultDetailsComponent implements OnInit {
+
+  result: Result;
+
+  constructor( private dataService: DataService,
+               private route: ActivatedRoute,
+               private location: Location) { }
+
+  ngOnInit() {
+    this.route.params.pipe(
+      switchMap(
+        (params: Params) =>
+          this.dataService.getResultByResultId(+params['idResult'])
+      )).subscribe(result => this.result = result);
+  }
+
+}
