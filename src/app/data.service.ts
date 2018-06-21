42ad3784eca082182ff21b8972c6c0d0deeb35d2
warrantEagle <aro_1595@wp.pl>
2018-06-21 16:44:48 +0200

    alert remove.

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -30,7 +30,7 @@ export class DataService {
   }
 
   getPerson(login: string) {
-    alert('Person get: !');
+   // alert('Person get: !');
     return this.http.get<Person>(this.peopleUrl + '/login/' + login + '/' + login );
   }
 
@@ -65,7 +65,6 @@ export class DataService {
 
   createPoints(points: Point[]) {
 
-    alert('Length: !' + points.length);
     return this.http.post<Point[]>(this.resultUrl + '/createPoints', points);
   }
   getPointsByResultId(idResult: number) {
